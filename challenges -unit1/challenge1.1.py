0f6b54a0db391e23c75bda512dc7863ad8b2d5fc
aluau131322128035 <146135234+aluau131322128035@users.noreply.github.com>
2023-09-27 02:19:06 +0530

    Add files via upload

---
+++ challenges -unit1/challenge1.1.py
@@ -0,0 +1,15 @@
+#1.1 Implement a recursion function to calculate the factorial of a given number.
+
+def factorial(n):
+     
+    if (n==1 or n==0):
+         
+        return 1
+     
+    else:
+         
+        return (n * factorial(n - 1))
+ 
+num = 5;
+print("number : ",num)
+print("Factorial : ",factorial(num))
